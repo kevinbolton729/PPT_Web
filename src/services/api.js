7195e6cf5463fce6015dc2c4341c89aeb2f31e8a
kevinbolton729 <kbolton@sohu.com>
2018-05-08 10:54:43 +0800

    edit
    edit api

--- src/services/api.js
+++ src/services/api.js
@@ -15,66 +15,7 @@ export async function getHomeData(params = {}) {
     params
   });
 }
-// 获取栏目隐射表
-export async function getChannelTypes(params = {}) {
-  return request(`${API_DOMAIN}/api/front/allchanneltype`, {
-    params
-  });
-}
-// 获取文章列表
-// export async function getArticleList(params = {}) {
-//   return request(`${API_DOMAIN}/api/front/allarticles`, {
-//     params
-//   });
-// }
-// 获取文章列表
-export async function getArticleListByid(params = {}) {
-  return request(`${API_DOMAIN}/api/front/articlebycid`, {
-    params
-  });
-}
-// 获取文章详细内容
-export async function getArticleDetail(params = {}) {
-  return request(`${API_DOMAIN}/api/front/articledetail`, {
-    params
-  });
-}
-// 获取产品列表
-export async function getProductListByid(params = {}) {
-  return request(`${API_DOMAIN}/api/front/articlebycid`, {
-    params
-  });
-}
-// 获取产品列详细内容
-export async function getProductDetail(params = {}) {
-  return request(`${API_DOMAIN}/api/front/articledetail`, {
-    params
-  });
-}
-// 获取实体店铺列表
-export async function getShopList(params = {}) {
-  return request(`${API_DOMAIN}/api/front/allshops`, {
-    params
-  });
-}
-// 获取舒览品牌列表
-export async function getBrandList(params = {}) {
-  return request(`${API_DOMAIN}/api/front/allbrands`, {
-    params
-  });
-}
-// 获取导航栏目列表
-export async function getChannelList(params = {}) {
-  return request(`${API_DOMAIN}/api/front/allchannels`, {
-    params
-  });
-}
+
 
 // [POST]
-// 添加加盟申请
-export async function addApply(params = {}) {
-  return request(`${API_DOMAIN}/api/front/addapply`, {
-    method: "POST",
-    body: params
-  });
-}
+
